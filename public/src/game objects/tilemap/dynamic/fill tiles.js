3aabc4ee091ef46fe41bb5e76c87e8ec10cc545c
ucabaur <ambroisel@theodo.co.uk>
2020-03-01 14:48:12 +0000

    Image update

--- public/src/game objects/tilemap/dynamic/fill tiles.js
+++ public/src/game objects/tilemap/dynamic/fill tiles.js
@@ -37,7 +37,7 @@ function getUpdatedPolutionValue() {
 
 function preload ()
 {
-    this.load.image('tiles', 'assets/tilemaps/tiles/tmw_desert_spacing.png');
+    this.load.image('tiles', 'assets/tilemaps/tiles/map5.png');
     this.load.tilemapTiledJSON('map', 'assets/tilemaps/maps/desert.json');
 }
 
